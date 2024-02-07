eceac0d8b7e6a14bb0c3b3b06fa1f55f1cc19d6a
FunkyFr3sh <FunkyFr3sh@users.noreply.github.com>
2024-02-07 03:02:17 +0100

    Add files via upload

---
+++ uninitialized-variable-crash-fix.ps1
@@ -0,0 +1,26 @@
+$bytes = [System.IO.File]::ReadAllBytes("Warcraft II BNE.exe");
+
+# Fixes random crash on startup caused by an uninitialized variable
+# See "uninitialized-variable-crash-fix.ERR" for the matching error log file 
+
+$bytes[0x0001C729] = 0x8D;
+$bytes[0x0001C72A] = 0x4C;
+$bytes[0x0001C72B] = 0xE4;
+$bytes[0x0001C72C] = 0x04;
+$bytes[0x0001C72D] = 0xEB;
+$bytes[0x0001C72E] = 0x19;
+$bytes[0x0001C748] = 0xC7;
+$bytes[0x0001C749] = 0x01;
+$bytes[0x0001C74A] = 0x00;
+$bytes[0x0001C74B] = 0x00;
+$bytes[0x0001C74C] = 0x00;
+$bytes[0x0001C74D] = 0x00;
+$bytes[0x0001C74E] = 0xEB;
+$bytes[0x0001C74F] = 0x11;
+$bytes[0x0001C75D] = 0xEB;
+$bytes[0x0001C75E] = 0xCA;
+$bytes[0x0001C75F] = 0x90;
+$bytes[0x0001C760] = 0x90;
+
+[System.IO.File]::WriteAllBytes("Warcraft II BNE.exe", $bytes);
+pause
