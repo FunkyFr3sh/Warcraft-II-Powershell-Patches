62ffac75cab093483c9e561b85222e42b9e4dbff
FunkyFr3sh <FunkyFr3sh@users.noreply.github.com>
2024-02-07 16:27:46 +0100

    Update uninitialized-variable-crash-fix.ps1

--- uninitialized-variable-crash-fix.ps1
+++ uninitialized-variable-crash-fix.ps1
@@ -3,12 +3,17 @@ $bytes = [System.IO.File]::ReadAllBytes("Warcraft II BNE.exe");
 # Fixes random crash on startup caused by an uninitialized variable
 # See "uninitialized-variable-crash-fix.ERR" for the matching error log file 
 
+# Patch: Set uninitialized variable to 0
+
+# 0x0041C729
 $bytes[0x0001C729] = 0x8D;
 $bytes[0x0001C72A] = 0x4C;
 $bytes[0x0001C72B] = 0xE4;
 $bytes[0x0001C72C] = 0x04;
 $bytes[0x0001C72D] = 0xEB;
 $bytes[0x0001C72E] = 0x19;
+
+# 0x0041C748
 $bytes[0x0001C748] = 0xC7;
 $bytes[0x0001C749] = 0x01;
 $bytes[0x0001C74A] = 0x00;
@@ -17,6 +22,8 @@ $bytes[0x0001C74C] = 0x00;
 $bytes[0x0001C74D] = 0x00;
 $bytes[0x0001C74E] = 0xEB;
 $bytes[0x0001C74F] = 0x11;
+
+# 0x0041C761
 $bytes[0x0001C75D] = 0xEB;
 $bytes[0x0001C75E] = 0xCA;
 $bytes[0x0001C75F] = 0x90;
